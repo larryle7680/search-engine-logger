18045867a58566a2058e25dc6d5a109189b403b6
Larry <larryle7680@gmail.com>
2026-04-22 09:59:15 -0500

    Created a menu and got a variable for the users Choice

---
+++ src/main/java/com/pluralsight/SearchEngineLoggerApp.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+import java.io.FileWriter;
+import java.util.*;
+
+public class SearchEngineLoggerApp {
+    static Scanner theScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //Prompt the user to choose an option and store the value
+        System.out.println("=== Main Menu ===");
+        System.out.println();
+        System.out.println("1. Launch the Application");
+        System.out.println("2. Search for term");
+        System.out.println("3. Exit the Program");
+        System.out.println();
+        System.out.print("Please select the option: ");
+        int usersChoice = theScanner.nextInt();
+
+        //Creating the menu
+        //Variable for menu running
+        boolean isRunning = true;
+        while(isRunning){
+
+
+        }
+
+    }
+
+    public static void logger(){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
